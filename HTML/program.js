76b5a41ee2729350bbb741600a6e996aab25187d
kvelraj_concord <kvelraj@concord.net>
2022-04-13 10:17:33 +0530

    JS

---
+++ HTML/program.js
@@ -0,0 +1,14 @@
+// Number -> Any numerical value from Positive, Negative, Fractional, Decimal
+
+// Syntax To create a Variable (Temp Storage)
+
+// var variable_name = value;
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var age = 28;
+console.log(age);
+
+// String -> Any text value which is typed inside '' or ""
+
+var email_id = "karthick@gmail.com";
+console.log(email_id);
