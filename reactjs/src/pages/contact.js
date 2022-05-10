7cda194c0c12e54af41a328f0c0813c867d3fa7b
kvelraj_concord <kvelraj@concord.net>
2022-05-10 10:51:20 +0530

    routing

--- reactjs/src/pages/contact.js
+++ reactjs/src/pages/contact.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const ContactPage = () => {
   return(
     <div>
       <h1>This is a Contact Page</h1>
+      <NavLink to="/">Go to HomePage</NavLink>
+      <br></br>
+      <NavLink to="/about">Click to go About Page</NavLink>
     </div>
   )
 }
--- reactjs/src/pages/home.js
+++ reactjs/src/pages/home.js
@@ -1,9 +1,11 @@
 import React  from "react";
+import {NavLink} from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is a Home Page</h1>
+      <NavLink to="/contact">Go to Contact Page</NavLink>
     </div>
   )
 }
