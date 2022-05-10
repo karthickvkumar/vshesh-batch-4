d4abb129f230fb83d56d0756af96441657727383
kvelraj_concord <kvelraj@concord.net>
2022-05-10 10:23:35 +0530

    routing

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import LoginForm from "./components/login";
 import RegisterPage from "./components/register";
+import HomePage from "./pages/home";
+import ContactPage from "./pages/contact";
+import AboutPage from "./pages/about";
 
 import "./css/style.css";
 
@@ -10,8 +13,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="login" element={<LoginForm></LoginForm>}></Route>
         <Route path="register" element={<RegisterPage></RegisterPage>}></Route>
+        <Route path="contact" element={<ContactPage></ContactPage>}></Route>
+        <Route path="about" element={<AboutPage></AboutPage>}></Route>
       </Routes>
     </BrowserRouter>
   )
