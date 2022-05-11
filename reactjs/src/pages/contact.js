6e9e8e56cbae73d0b6fa5e27b6dc994b2cf96828
kvelraj_concord <kvelraj@concord.net>
2022-05-11 10:15:40 +0530

    API

--- reactjs/src/pages/contact.js
+++ reactjs/src/pages/contact.js
@@ -7,9 +7,16 @@ const ContactPage = () => {
       <h1>This is a Contact Page</h1>
       <NavLink to="/">Go to HomePage</NavLink>
       <br></br>
+      {/* Clustor, Indexing
+How to skip first 5 records from a collection
+.live and .bind
+method chaining
+web workers
+test_user7e017@concord.net */}
       <NavLink to="/about">Click to go About Page</NavLink>
     </div>
   )
 }
 
 export default ContactPage;
+
--- reactjs/src/pages/home.js
+++ reactjs/src/pages/home.js
@@ -1,10 +1,25 @@
 import React  from "react";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 const HomePage = () => {
+
+  const listUser = () => {
+    var url = "https://reqres.in/api/users?page=2";
+    axios.get(url)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+
+  }
+
   return (
     <div>
       <h1>This is a Home Page</h1>
+      <button onClick={() => listUser()}>List the User Profile</button>
       <NavLink to="/contact">Go to Contact Page</NavLink>
     </div>
   )
