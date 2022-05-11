91190e211b2f41a0e5bc07dd761cd6142d48173f
kvelraj_concord <kvelraj@concord.net>
2022-05-11 10:49:51 +0530

    GET Api

--- reactjs/src/pages/home.js
+++ reactjs/src/pages/home.js
@@ -1,26 +1,55 @@
-import React  from "react";
+import React, {useState}  from "react";
 import {NavLink} from "react-router-dom";
 import axios from "axios";
 
 const HomePage = () => {
 
+  const [userList, setUserList] = useState([]);
+
   const listUser = () => {
     var url = "https://reqres.in/api/users?page=2";
     axios.get(url)
           .then((response) => {
-            console.log(response);
+            var serverData = response.data;
+            setUserList(serverData.data);
           })
           .catch((error) => {
             console.log(error);
           })
-
   }
+
+  var userListData = userList.map((value, index) => {
+    return(
+      <tr key={index}>
+        <td>{value.first_name}</td>
+        <td>{value.last_name}</td>
+        <td>{value.email}</td>
+        <td>
+          <img src={value.avatar} />
+        </td>
+      </tr>
+    )
+  })
 
   return (
     <div>
       <h1>This is a Home Page</h1>
       <button onClick={() => listUser()}>List the User Profile</button>
       <NavLink to="/contact">Go to Contact Page</NavLink>
+      <table id="customers">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {userListData}
+        </tbody>
+      </table>
     </div>
   )
 }
